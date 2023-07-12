f2649fe59088c18d4760ed71331c69a87c628405
ab-intel737 <138695989+ab-intel737@users.noreply.github.com>
2023-07-12 14:12:33 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    base: "/new-rep-frm-cwa-template-web-app/",  
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
   themeConfig: {
